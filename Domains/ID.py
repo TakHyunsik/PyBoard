264fd3a2b6fb9d2a807d9d7a848320866174af82
taktakgyun <vbmrkk@naver.com>
2024-05-04 15:56:14 +0900

    ♻️ : split str_uuid

--- Domains/ID.py
+++ Domains/ID.py
@@ -16,40 +16,42 @@ class ID(metaclass=ABCMeta):
 
 class IIDBuilder(metaclass=ABCMeta):
     @abstractmethod
-    def set_seqence(self, seq: int) -> Self: ...
-
-    @abstractmethod
     def set_uuid(self, uuid_hex: Optional[str] = None) -> Result[Self, str]: ...
+    @abstractmethod
+    def set_seq(self, seq: int) -> Self: ...
 
 @dataclass
 class IDBuilder(IIDBuilder):
     uuid: Optional[UUID] = None
     seq: Optional[int] = None
 
-    def set_seqence(self, seq: int) -> Self:
+    def set_seq(self, seq: int) -> Self:
         assert isinstance(seq, int), "Type of seq is int."
         assert self.seq is None, "The sequence is already set."
         assert seq >= 0, "seq >= 0"
 
         self.seq = seq
         return self
-
-    def set_uuid(self, uuid: Union[UUID, str, None] = None) -> Result[Self, str]:
+    
+    def set_str_uuid(self, uuid: str) -> Result[Self, str]:
         assert self.uuid is None, "The uuid_hex is already set."
+        assert check_hex_string(uuid), "The uuid_hex is not in hex format."
+        if not check_hex_string(uuid):
+            return Err("not hex format")
+        try:
+            self.uuid = UUID(hex=uuid)
+        except:
+            return Err("Not Convert UUID")
+        return Ok(self)
+
+    def set_uuid(self, uuid: Optional[UUID] = None) -> Self:
+        assert self.uuid is None, "The uuid is already set."
         match uuid:
             case None:
                 self.uuid = uuid4()
-            case k if isinstance(uuid, str):
-                assert check_hex_string(k), "The uuid_hex is not in hex format."
-                if not check_hex_string(k):
-                    return Err("not hex format")
-                try:
-                    self.uuid = UUID(hex=uuid)
-                except:
-                    return Err("Not Convert UUID")
             case id if isinstance(id, UUID):
                 self.uuid = id
             case _:
-                assert False, "Type of uuid_hex is str."
+                assert False, "Type of uuid is not str."
 
-        return Ok(self)
+        return self
